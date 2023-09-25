8fecd60ba14be7234c1ed40a305c728e4cd96588
Mst.Shorifa <shorifaakter825@gmail.com>
2023-09-25 23:47:48 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+    <!-- font-awesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <!-- bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="row text-center py-5">
+         <div class="col-md-3 col-sm-6 my-3 my-md-0">
+           <form action="index.php" method="post">
+            <div class="card shadow">
+                <div>
+                <img src="./upload/product1.png" alt="image1" class="img-fluid card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Product1</h5>
+                    <h6>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </h6>
+                </div>
+                </div>
+            </div>
+           </form>
+         </div>
+         <div class="col-md-3 col-sm-6 my-3 my-md-0">
+            
+         </div>
+         <div class="col-md-3 col-sm-6 my-3 my-md-0">
+            
+         </div>
+         <div class="col-md-3 col-sm-6 my-3 my-md-0">
+            
+         </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+  </body>
+</body>
+</html>
